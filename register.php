0b2ef3b396abacf87b64ba5c2f0baa4a0d0e3c49
gotaa503 <138383499+gotaa503@users.noreply.github.com>
2024-07-13 18:00:15 +0500

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,21 @@
-
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $users = file('users.txt', FILE_IGNORE_NEW_LINES);
+    $isValid = false;
+    foreach ($users as $user) {
+        list($storedUsername, $storedPassword) = explode(':', $user);
+        if ($storedUsername === $username && $storedPassword === $password) {
+            $isValid = true;
+            break;
+        }
+    }
+    if ($isValid) {
+        echo 'Вход успешен!';
+    } else {
+        echo 'Неверное имя пользователя или пароль.';
+    }
+}
+?>
+  
